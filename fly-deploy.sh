a5dcd993ce370c4651cde2ffcc53d1746c4dfae6
shayneczyzewski <shayne.czyzewski@gmail.com>
2023-01-03 15:01:52 -0500

    use urandom

--- fly-deploy.sh
+++ fly-deploy.sh
@@ -118,7 +118,7 @@ launchServer() {
   flyctl launch --no-deploy --name "$server_name" --region "$region" || exit
   cp -f fly.toml "$WASP_PROJECT_DIR/fly-server.toml"
 
-  random_string=$(od -x /dev/random | head -1 | awk '{print $2$3$4$5$6$7$8$9}')
+  random_string=$(od -x /dev/urandom | head -1 | awk '{print $2$3$4$5$6$7$8$9}')
   flyctl secrets set JWT_SECRET="$random_string" PORT=8080 WASP_WEB_CLIENT_URL="$client_url" || exit
 
   flyctl postgres create --name "$db_name" --region "$region" || exit
